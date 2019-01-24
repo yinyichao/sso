04f5ef12a1d2b31e084b25f5cc9cd7b946d577c8
yinyichao <yinyichao@163.com>
2019-01-24 18:07:21 +0800

    first commit

---
+++ parent/src/main/java/com/founder/parent/ParentApplication.java
@@ -0,0 +1,14 @@
+package com.founder.parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParentApplication.class, args);
+	}
+
+}
+
